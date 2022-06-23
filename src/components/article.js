f9a22267483f820a94fee15de1425f91709f10ff
David Paul Graham <dpgraham4401@gmail.com>
2022-06-22 22:12:53 -0400

    initial commit

---
+++ src/components/article.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Article = () => {
+    return (
+    <div className="container">
+        <div className="row">
+            <div className="col d-flex justify-content-center">
+            </div>
+        </div>
+        <div className="row my-5">
+            <div className="col-md-9">
+                <div className="card">
+                    <div className="card-header bg-dpg-lightgray">
+                    </div>
+                    <div className="card-body text-body px-4 bg-light">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-5">
+        </div>
+    </div>
+    )
+}
+
+export default Article
