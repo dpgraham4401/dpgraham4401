a4b6c9bc2675e853e94a502c1c624278b73bc368
David Paul Graham <dpgraham4401@gmail.com>
2022-06-23 07:22:28 -0400

    navbar boilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Article from "./components/article";
+import NavBtn from "./components/nav";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-          <div className="min-vh-100 bg-dark">
-              <Article/>
-          </div>
+    <div className="min-vh-100 bg-dark">
+      <NavBtn/>
+      <Article/>
+    </div>
   );
 }
 
