138cc90b5b60dcd3d5142d0213fd9e188639bca7
David Paul Graham <dpgraham4401@gmail.com>
2022-06-23 22:24:07 -0400

    initial custom navbar btn

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,33 +1,29 @@
 import React from "react";
-import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
+import {Navbar, Container, Nav} from "react-bootstrap";
 
 const NavBtn = () => {
   return (
     <Navbar bg="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand className="bg-dpg-teal bg-gradient rounded-circle"
+                      href="#home">
           <img
             src="/rp_bg_trans.png"
-            width="60"
-            height="60"
-            className="d-inline-block align-top"
+            width="120"
+            height="100"
             alt="DPGraham logo"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle
+          className="p-0 border-0 rounded-circle"
+          aria-controls="basic-navbar-nav">
+          <p
+            className="bg-dpg-teal rounded-circle px-4 py-2 h3 fw-bolder">X</p>
+        </Navbar.Toggle>
+        <Navbar.Collapse className="" id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another
-                action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider/>
-              <NavDropdown.Item href="#action/3.4">Separated
-                link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link className="text-white h4" href="#home">Home</Nav.Link>
+            <Nav.Link className="text-white h4" href="#link">Link</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
