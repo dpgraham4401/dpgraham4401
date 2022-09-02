30b02c6db7c677c9196c581089ff7950b0388b5e
David Graham <dpgraham4401@gmail.com>
2022-09-02 13:23:44 -0400

    render articles on homepage

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { api } from "../../services";
 import { Blog } from "../../types";
+import DpgCard from "../../components/DpgCard";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [blogs, setBlogs] = useState<Blog[] | undefined>(undefined);
@@ -11,7 +13,6 @@ function Home() {
     api
       .get("blog", null)
       .then((response) => {
-        console.log(response);
         setBlogs(response as Blog[]);
       })
       .then(() => {
@@ -21,23 +22,41 @@ function Home() {
   }, []);
   return (
     <Container>
-      <Card className="border-0">
-        <Card.Header as="h5" className="bg-light">
-          Home
-        </Card.Header>
-        <Card.Body>{loading ? printData(blogs) : "loading"}</Card.Body>
-      </Card>
+      <DpgCard>
+        <DpgCard.Header title="Welcome!" />
+        <DpgCard.Body>
+          {loading ? (
+            <DpgCard.Spinner />
+          ) : blogs ? (
+            listArticles(blogs)
+          ) : (
+            "error"
+          )}
+        </DpgCard.Body>
+      </DpgCard>
     </Container>
   );
 }
 
-function printData(blogs: Blog[] | undefined) {
-  if (blogs) {
-    blogs.map((blog) => console.log(blog.id));
-    return <p>hello</p>;
-  } else {
-    return <p>Good Bye</p>;
-  }
+function listArticles(blogs: Blog[]) {
+  return blogs.map((blog: Blog, i: number) => {
+    return (
+      <div key={i}>
+        <Link to={`blog/${blog.id}`} className="h3 text-decoration-none">
+          {blog.title}
+        </Link>
+        <p className="ms-2 mb-0 text-muted">{blog.createDate}</p>
+        <p className="mt-0">
+          {blog.content}
+          <span>
+            <Link className="text-decoration-none" to={`blog/${blog.id}`}>
+              {"read more..."}
+            </Link>
+          </span>
+        </p>
+      </div>
+    );
+  });
 }
 
 export default Home;
