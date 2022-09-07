e46822a4ba14ef24d21ce5faef34d6b9adc09262
David Graham <dpgraham4401@gmail.com>
2022-09-07 19:59:22 -0400

    move components inside router, add dummy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./features/Home";
-import TopNav from "./components/TopNav";
 import Article from "./features/Article";
 import BlogHome from "./features/BlogHome";
 
@@ -10,14 +8,14 @@ function App() {
   return (
     <>
       <main className="bg-dark flex-fill py-1">
-        <TopNav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<BlogHome />} />
-          <Route path="/blog/:id" element={<Article />} />
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<BlogHome />} />
+            <Route path="/blog/:id" element={<Article />} />
+          </Routes>
+        </BrowserRouter>
       </main>
-      <Footer />
     </>
   );
 }
--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -4,6 +4,8 @@ import { api } from "../../services";
 import { Blog } from "../../types";
 import DpgCard from "../../components/DpgCard";
 import { Link } from "react-router-dom";
+import Footer from "../../components/Footer";
+import TopNav from "../../components/TopNav";
 
 function Home() {
   const [blogs, setBlogs] = useState<Blog[] | undefined>(undefined);
@@ -23,20 +25,24 @@ function Home() {
       .catch((error) => console.log(error));
   }, []);
   return (
-    <Container>
-      <DpgCard>
-        <DpgCard.Header title="Welcome!" />
-        <DpgCard.Body>
-          {loading ? (
-            <DpgCard.Spinner />
-          ) : blogs ? (
-            listArticles(blogs)
-          ) : (
-            "error"
-          )}
-        </DpgCard.Body>
-      </DpgCard>
-    </Container>
+    <>
+      <TopNav />
+      <Container>
+        <DpgCard>
+          <DpgCard.Header title="Welcome!" />
+          <DpgCard.Body>
+            {loading ? (
+              <DpgCard.Spinner />
+            ) : blogs ? (
+              listArticles(blogs)
+            ) : (
+              "error"
+            )}
+          </DpgCard.Body>
+        </DpgCard>
+      </Container>
+      <Footer />
+    </>
   );
 }
 
