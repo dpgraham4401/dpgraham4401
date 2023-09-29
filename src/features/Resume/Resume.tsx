7fb82ee291f37887b6c6d1f85a66d9ffc6d47654
David Paul Graham <dpgraham4401@gmail.com>
2023-09-29 14:00:00 +0100

    add download button on resume page

--- src/features/Resume/Resume.tsx
+++ src/features/Resume/Resume.tsx
@@ -1,6 +1,17 @@
+import { Download } from "@mui/icons-material";
+import { Box, Fab } from "@mui/material";
 import { CloudChallengeResume } from "components/CloudChallengeResume";
 import { useTitle } from "hooks";
 import React from "react";
+
+const fabStyle = {
+  margin: 0,
+  top: "auto",
+  right: 20,
+  bottom: 20,
+  left: "auto",
+  position: "fixed",
+};
 
 /**
  * This Resume is, primarily in HTML and css as part of the cloud resume challenge.
@@ -9,5 +20,20 @@ import React from "react";
 export function Resume() {
   // ToDo - Add a way to download PDF resume
   useTitle("Resume");
-  return <CloudChallengeResume />;
+  return (
+    <Box p={2}>
+      <CloudChallengeResume />
+      <Fab
+        sx={fabStyle}
+        color="success"
+        variant="extended"
+        href={`${
+          import.meta.env.VITE_SITE_URL
+        }static/resume/DavidGrahamResume.pdf`}
+      >
+        <Download />
+        Download pdf
+      </Fab>
+    </Box>
+  );
 }
