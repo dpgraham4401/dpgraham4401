1601ef4501239d2446d79b936464c6b466181d3e
David Paul Graham <dpgraham4401@gmail.com>
2025-03-30 09:26:44 -0400

    POC rewriting component as vue component

---
+++ src/components/common/Section.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="p-4 max-w-2xl mx-auto">
+    <slot />
+  </section>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* Add any scoped styles here if needed */
+</style>
