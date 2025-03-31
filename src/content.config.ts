7366c2478056e863d3da5e054bcc56d33627f4ba
David Graham <dpgraham4401@gmail.com>
2025-03-30 21:52:21 -0400

    add tsignore to content config

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,15 @@
  * See Astros docs on Content Collections https://docs.astro.build/en/guides/content-collections/
  */
 
-import {glob} from 'astro/loaders';
-import {defineCollection, z} from "astro:content";
+import { glob } from "astro/loaders";
+// @ts-ignore - not sure why this is giving warning, not causing problems
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "src/posts" }),
   schema: z.object({
-      title: z.string(),
-      description: z.string(),
+    title: z.string(),
+    description: z.string()
   })
 });
 
