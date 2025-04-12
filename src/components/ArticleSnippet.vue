2e23894f1af496b6360a85a629fa1415b07a49cc
David Graham <dpgraham4401@gmail.com>
2025-04-12 12:20:51 -0400

    move features articles and article snippet to vue

---
+++ src/components/ArticleSnippet.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import Anchor from "./common/Anchor.vue";
+
+type Props = {
+  title: string;
+  description: string;
+  url: string;
+  duration?: string;
+  timestamp?: string;
+};
+
+defineProps<Props>();
+
+</script>
+
+<template>
+  <div class="zag-text zag-transition flex flex-col gap-3 pb-8">
+    <Anchor class="text-xl" url={url}>
+      {title}
+    </Anchor>
+    <p class="">
+      {{ description }}
+    </p>
+    <div
+      class="zag-muted zag-transition flex justify-between items-center"
+    >
+      <p v-if="duration">
+        {{ duration }}
+      </p>
+    </div>
+  </div>
+</template>
