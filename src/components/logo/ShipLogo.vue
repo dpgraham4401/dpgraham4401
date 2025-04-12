eab2f1efbeada8954c56324c567d14ca788862db
David Graham <dpgraham4401@gmail.com>
2025-04-12 19:10:56 -0400

    move logo to separate component

---
+++ src/components/logo/ShipLogo.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <a aria-label="home" href="/">
+    <div class="bg-white rounded-full">
+      <img alt="Logo" src="/img/logo.svg" style="width: 75px; height: 75px;" />
+    </div>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
