41deff5a7645701ad5100fe09d9d8a41ca4427cd
David Graham <dpgraham4401@gmail.com>
2025-04-12 13:10:11 -0400

    use glob again to get blog content
    maybe best to fix in the future and move back to src/content/blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,9 +2,11 @@
  * See Astros docs on Content Collections https://docs.astro.build/en/guides/content-collections/
  */
 
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "src/content/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
