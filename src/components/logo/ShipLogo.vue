1e4293c6bff300ca43d6423c34f061e68dc8edeb
David Graham <dpgraham4401@gmail.com>
2025-04-12 20:51:26 -0400

    Modify the css waves so the ship logo doesn't float in mid air

--- src/components/logo/ShipLogo.vue
+++ src/components/logo/ShipLogo.vue
@@ -1,23 +1,27 @@
 <template>
   <div
-    class="header bg-white rounded-full w-28 h-16 relative overflow-hidden flex items-center justify-center">
+    class="header bg-white rounded-full w-28 h-16 relative overflow-hidden flex items-start justify-center">
     <!--Waves Container-->
     <div>
-      <div class="pt-4">
-        <img alt="Logo" class="w-20 h-1/4 logo-bob" src="/img/logo.svg" />
+      <div>
+        <img alt="Logo" class="w-20 h-1/2 logo-bob-rotate" src="/img/pirate_ship_full_bottom.svg" />
       </div>
-      <svg class="absolute bottom-0 left-0 w-full h-1/3" preserveAspectRatio="none"
+      <svg class="absolute bottom-0 left-0 w-full h-2/5" preserveAspectRatio="none"
            shape-rendering="auto"
            viewBox="0 24 150 28" xmlns="http://www.w3.org/2000/svg"
            xmlns:xlink="http://www.w3.org/1999/xlink">
         <defs>
           <path id="gentle-wave"
                 d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+          <path id="gentle-wave-long"
+                d="M-200 44c40 0 80-10 120-10 s80 10 120 10 80-10 120-10 80 10 120 10 v44h-480z" />
         </defs>
         <g class="parallax">
           <use class="fill-blue-300" x="0" xlink:href="#gentle-wave" y="0" />
-          <use class="fill-blue-400" x="28" xlink:href="#gentle-wave" y="3" />
-          <use class="fill-blue-500" x="48" xlink:href="#gentle-wave" y="7" />
+          <use class="fill-blue-400" x="48" xlink:href="#gentle-wave" y="3" />
+          <use class="fill-blue-500" x="88" xlink:href="#gentle-wave" y="7" />
+          <use class="fill-blue-200" x="0" xlink:href="#gentle-wave-long" y="-5" />
+          <use class="fill-blue-900" x="0" xlink:href="#gentle-wave-long" y="-3" />
         </g>
       </svg>
     </div>
@@ -26,22 +30,51 @@
 </template>
 
 <style scoped>
-@keyframes bobUpDown {
+/* Bob up/down + rotate */
+@keyframes bobAndTilt {
   0% {
-    transform: translateY(0);
+    transform: translateY(0) rotate(0deg);
+  }
+  15% {
+    /* Slightly up, small clockwise tilt */
+    transform: translateY(-3px) rotate(8deg);
+  }
+  30% {
+    /* Up more, a bit more tilt */
+    transform: translateY(-5px) rotate(10deg);
+  }
+  40% {
+    /* Closer to peak, reduce tilt slightly */
+    transform: translateY(-7px) rotate(6deg);
   }
   50% {
-    transform: translateY(-10px);
+    /* Peak: fully up, flat */
+    transform: translateY(-10px) rotate(0deg);
+  }
+  60% {
+    /* Coming down, slight counterclockwise tilt */
+    transform: translateY(-7px) rotate(-6deg);
+  }
+  70% {
+    /* Further down, increase counterclockwise tilt */
+    transform: translateY(-5px) rotate(-10deg);
+  }
+  85% {
+    /* Almost bottom, lighten tilt */
+    transform: translateY(-3px) rotate(-8deg);
   }
   100% {
-    transform: translateY(0);
+    /* Back at bottom, flat */
+    transform: translateY(0) rotate(0deg);
   }
 }
 
-/* Attach the bob animation to the logo */
-.logo-bob {
-  animation: bobUpDown 4s ease-in-out infinite;
+
+/* Attach the bob+tilt animation to the logo */
+.logo-bob-rotate {
+  animation: bobAndTilt 4s ease-in-out infinite;
 }
+
 
 .parallax > use {
   animation: move-forever 25s cubic-bezier(.55, .5, .45, .5) infinite;
@@ -60,6 +93,11 @@
 .parallax > use:nth-child(3) {
   animation-delay: -5s;
   animation-duration: 13s;
+}
+
+.parallax > use:nth-child(4) {
+  animation-delay: -5s;
+  animation-duration: 20s;
 }
 
 @keyframes move-forever {
