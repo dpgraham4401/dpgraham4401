1704e12fb9d73e28c0645d05eda79a5bd14e9c6e
David Paul Graham <dpgraham4401@gmail.com>
2025-04-19 11:27:19 -0400

    refactor strucutre of global data into objects to create room for more items and use a structured approach to hold data

--- src/components/home/FeaturedProjects.vue
+++ src/components/home/FeaturedProjects.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { GLOBAL } from "@/lib/variables";
+import { GLOBAL } from "@/lib/constants.ts";
 import { getCollection } from "astro:content";
 import Anchor from "src/components/common/Anchor.vue";
 import ProjectSnippet from "src/components/ProjectSnippet.vue";
@@ -12,17 +12,17 @@ const featuredProjects = posts.filter((post) => post.data.isFeatured);
   <div class="flex justify-between items-center w-full">
     <!-- Display the heading and link from GLOBAL -->
     <h3 class="font-display text-lg sm:text-xl leading-loose">
-      {{ GLOBAL.projectsName }}
+      {{ GLOBAL.projects.title }}
     </h3>
     <Anchor ariaLabel="view all" class="text-base" url="/projects">
-      {{ GLOBAL.viewAll }}
+      View All
     </Anchor>
   </div>
 
   <ul class="my-8">
     <!-- Show a fallback message if no projects are found -->
     <p v-if="featuredProjects.length === 0">
-      {{ GLOBAL.noProjects }}
+      No Featured Projects Found
     </p>
 
     <!-- Otherwise, iterate through the featuredProjects array -->
